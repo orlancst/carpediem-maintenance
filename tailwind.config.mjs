c831903cec29b7f5c77261a2afa7dc0192eb11e1
orlancst <orlancssst@gmail.com>
2023-12-28 16:21:07 -0500

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'fondo-carpediem': "url('./src/assets/img/fondo.jpg')",
+			},
+			fontFamily: {
+				'open-sans': '"Open Sans", sans-serif',
+				'gill-sans': '"Gill Sans", sans-serif',
+				'gill-sans-medium': '"Gill Sans Medium", sans-serif',
+				'gill-sans-bold': '"Gill Sans Bold", sans-serif',
+				
+			},
+			
+		},
+	},
+	plugins: [],
+}
